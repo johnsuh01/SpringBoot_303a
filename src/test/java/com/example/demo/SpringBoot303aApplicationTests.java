9f740eec2b2f7bb64c11547c5d58932be11d431a
johnsuh01 <johnsuh01@hotmail.com>
2019-12-12 17:32:31 -0500

    SpringBoot_303

---
+++ src/test/java/com/example/demo/SpringBoot303aApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot303aApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
